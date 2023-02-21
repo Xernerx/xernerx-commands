01f3739cd77b40bb74f881f13c377bcceba4ed5a
Dummi <raydecrom@hotmail.com>
2023-02-21 19:30:26 +0100

    Patched an error in haste

--- src/commands/documentation.ts
+++ src/commands/documentation.ts
@@ -1,29 +1,29 @@
 import { MessageCommandBuilder } from 'xernerx';
 
 export default class DocumentationCommand extends MessageCommandBuilder {
-	constructor() {
-		super('documentation', {
-			name: 'documentation',
-			aliases: ['docs'],
-			owner: true,
-			description: 'A command to read the documentation of Discord.js or xernerx.',
-			category: 'XernerxCommand',
-			prefix: [],
-			args: [
-				{
-					name: 'option',
-					type: 'option',
-					content: ['x', 'xernerx', 'djs', 'discord.js'],
-				},
-				{
-					name: 'rest',
-					type: 'rest',
-				},
-			],
-		});
-	}
+    constructor() {
+        super('documentation', {
+            name: 'documentation',
+            aliases: ['docs'],
+            owner: true,
+            description: 'A command to read the documentation of Discord.js or xernerx.',
+            category: 'XernerxCommand',
+            prefix: [],
+            args: [
+                {
+                    name: 'option',
+                    type: 'option',
+                    content: ['x', 'xernerx', 'djs', 'discord.js'],
+                },
+                {
+                    name: 'rest',
+                    type: 'rest',
+                },
+            ],
+        });
+    }
 
-	async exec(message: any, args: any) {
-		message.util.reply('Soon:tm:');
-	}
+    async exec(message: any, args: any) {
+        message.util.reply('Soon:tm:');
+    }
 }
--- src/tools/package.ts
+++ src/tools/package.ts
@@ -1,37 +1,48 @@
 export default {
-	"name": "xernerx-commands",
-	"version": "0.0.10",
-	"type": "module",
-	"description": "An extension for the xernerx framework containing a collection of commands useful for when developing a bot.",
-	"main": "dist/main.js",
-	"keywords": [
-		"xernerx"
-	],
-	"author": "Dummi",
-	"license": "MIT",
-	"homepage": "https://thedummi.github.io/xernerx/home.html",
-	"packageManager": "npm@8.5.5",
-	"scripts": {
-		"start": "npm i && npm fund && npm audit fix --force && npm link && tsc -w",
-		"build": "node ../../.scripts/rewritePackage.js",
-		"patch": " npm run build && tsc && git add . && npm run commit && npm version patch && npm publish && git push",
-		"minor": " npm run build && tsc && git add . && npm run commit && npm version minor && npm publish && git push",
-		"major": " npm run build && tsc && git add . && npm run commit && npm version major && npm publish && git push",
-		"commit": "node ../../.scripts/commit.js"
-	},
-	"bugs": {
-		"url": "https://github.com/TheDummi/xernerx-commands/issues"
-	},
-	"repository": {
-		"type": "git",
-		"url": "git+https://github.com/TheDummi/xernerx-commands.git"
-	},
-	"dependencies": {
-		"got": "^12.5.3",
-		"xernerx": "^3.3.5"
-	},
-	"devDependencies": {
-		"@types/node": "^18.11.18",
-		"typescript": "^4.9.4"
-	}
+    "name": "xernerx-commands",
+    "version": "0.0.10",
+    "type": "module",
+    "description": "An extension for the xernerx framework containing a collection of commands useful for when developing a bot.",
+    "main": "dist/main.js",
+    "keywords": [
+        "xernerx"
+    ],
+    "author": "Dummi",
+    "license": "MIT",
+    "homepage": "https://thedummi.github.io/xernerx/home.html",
+    "packageManager": "npm@8.5.5",
+    "exports": {
+        "import": "./dist/main.js",
+        "require": "./dist/main.cjs",
+        "types": null,
+        "node": null,
+        "default": null
+    },
+    "scripts": {
+        "start": "npm i && npm audit fix --force && npm fund && npm link && tsc -w",
+        "patch": "npm run write && git add . && npm run commit && npm version patch && npm publish && git push",
+        "minor": "npm run write && git add . && npm run commit && npm version minor && npm publish && git push",
+        "major": "npm run write && git add . && npm run commit && npm version major && npm publish && git push",
+        "site": "git add docs && npm run commit && git push",
+        "commit": "node ../../.scripts/commit.js",
+        "write": "npm run build && node ../../.scripts/rewritePackage.js ",
+        "build": "tsc && npx tsup src/main.ts && npx prettier --write src"
+    },
+    "bugs": {
+        "url": "https://github.com/TheDummi/xernerx-commands/issues"
+    },
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/TheDummi/xernerx-commands.git"
+    },
+    "dependencies": {
+        "got": "^12.5.3",
+        "xernerx": "^3.3.5"
+    },
+    "devDependencies": {
+        "@types/node": "^18.11.18",
+        "prettier": "^2.8.4",
+        "tsup": "^6.6.3",
+        "typescript": "^4.9.4"
+    }
 }
