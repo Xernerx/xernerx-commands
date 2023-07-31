90032d82a564ce60c60c583cb17c7813c41af369
Dummi <raydecrom@hotmail.com>
2023-07-31 17:12:01 +0200

    0.1.6 - fixed xc! thingy

--- src/commands/main.ts
+++ src/commands/main.ts
@@ -1,4 +1,4 @@
-import { XernerxMessageCommand, version, Discord } from 'xernerx';
+import { XernerxMessageCommand, version, Discord, XernerxMessage } from 'xernerx';
 import { version as XCVersion } from '../main.js';
 
 export default class MainCommand extends XernerxMessageCommand {
@@ -26,10 +26,10 @@ export default class MainCommand extends XernerxMessageCommand {
             `XernerxCommands \`v${XCVersion}\`, Xernerx \`v${version}\`, Discord.js \`v${Discord.version}\`, \`Node ${process.version}\` on \`${
                 process.platform
             }\`.\nModules were loaded <t:${Math.round(message.client.readyTimestamp / 1000)}:R>, handlers were loaded <t:${Math.round(message.client.readyTimestamp / 1000)}:R>\n\nThis bot is ${
-                !message.client.shard ? 'not sharded' : `on ${message.client.shardCount} shard${message.client.shardCount > 1 ? 's' : ''}`
-            } and can see ${guilds.size} guild${guilds.size > 1 ? 's' : ''} and ${userCount} user${userCount > 1 ? 's' : ''}. Using ${await message.client.guilds.fetch(
+                message.client.options.shardCount === 0 ? 'not sharded' : `on shard ${message.client.options.shards[0]}`
+            } and can see ${guilds.size} guild${guilds.size > 1 ? 's' : ''} and ${userCount} user${userCount > 1 ? 's' : ''}. Using \`${await message.client.guilds.fetch(
                 message.client.settings.local
-            )} as local guild with owners ${(await Promise.all(message.client.settings.ownerId.map(async (o: any) => await message.client.users.fetch(o)))).join(', ')}\nMessage cache capped at ${
+            )}\` as local guild with owners ${(await Promise.all(message.client.settings.ownerId.map(async (o: any) => await message.client.users.fetch(o)))).join(', ')}\nMessage cache capped at ${
                 message.channel.messages.cache.maxSize
             }, presences intent is ${hasIntent('GuildPresences')}, members intent is ${hasIntent('GuildMembers')}, and message content intent is ${hasIntent(
                 'MessageContent'
